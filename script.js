e9912743b176925c0c435a80bdf650ec990a01be
malcodeman <malcodeman@users.noreply.github.com>
2017-06-01 22:55:15 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,99 @@
+// input_new_task operations
+function focusInputNewTask() {
+    "use strict";
+    document.getElementById("input_new_task").focus();
+}
+
+function clearInputNewTask() {
+    "use strict";
+    document.getElementById("input_new_task").value = "";
+}
+// Getters
+function getTask(task_number) {
+    "use strict";
+    return localStorage.getItem("task_" + task_number);
+}
+
+function getTaskCounter() {
+    "use strict";
+    return localStorage.getItem("task_counter");
+}
+// Task operations
+function setTask(new_task) {
+    "use strict";
+    localStorage.setItem("task_" + getTaskCounter(), new_task);
+}
+
+function setTaskCounter(new_counter) {
+    "use strict";
+    localStorage.setItem("task_counter", new_counter);
+}
+
+function removeTask(task_number) {
+    "use strict";
+    localStorage.removeItem("task_" + task_number);
+}
+var deleteTask = function () {
+    "use strict";
+    removeTask(this.id);
+    setTaskCounter(Number(getTaskCounter()) - 1);
+    document.getElementById("tasks_content").getElementsByClassName("task-item")[this.id].remove();
+    var i = Number(this.id);
+    for (i; i < Number(getTaskCounter()); i += 1) {
+        localStorage.setItem("task_" + i, getTask(i + 1));
+        document.getElementById("tasks_content").getElementsByClassName("task-item")[i].id = i;
+    }
+    focusInputNewTask();
+};
+
+function renderTask(task_number) {
+    "use strict";
+    var task_item, task_title;
+    task_item = document.createElement("div");
+    task_title = document.createElement("span");
+    task_item.id = task_number;
+    task_item.classList.add("task-item");
+    task_item.addEventListener("click", deleteTask);
+    task_title.classList.add("task-title");
+    task_title.textContent = getTask(task_number);
+    task_item.appendChild(task_title);
+    document.getElementById("tasks_content").appendChild(task_item);
+}
+var addTask = function (event) {
+    "use strict";
+    if (event.keyCode === 13 && this.value !== "") {
+        setTask(this.value);
+        renderTask(getTaskCounter());
+        setTaskCounter(Number(getTaskCounter()) + 1);
+        clearInputNewTask();
+    }
+};
+
+function renderTasks() {
+    "use strict";
+    var i = 0;
+    for (i; i < getTaskCounter(); i += 1) {
+        renderTask(i);
+    }
+}
+// Date operations
+function renderDate() {
+    "use strict";
+    var month_names, day_names, d;
+    month_names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    day_names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    d = new Date();
+    document.getElementById("date_text").textContent = day_names[d.getDay()] + ",  " + month_names[d.getMonth()] + " " + d.getDate();
+}
+
+function main() {
+    "use strict";
+    if (getTaskCounter() === null) {
+        setTaskCounter(0);
+    }
+    renderDate();
+    renderTasks();
+    document.getElementById("input_new_task").addEventListener("keyup", addTask);
+    focusInputNewTask();
+}
+main();
